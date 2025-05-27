1af246c2cfb2b9616a4a8f52d83f5841f6ba388a
MaxusTheOne <markus@bille.dk>
2025-05-27 11:57:26 +0200

    Reader works with mosaic images. V1.1.0

--- src/napari_czi_reader/czi_reader_CFIM.py
+++ src/napari_czi_reader/czi_reader_CFIM.py
@@ -3,9 +3,14 @@ import os
 
 import numpy as np
 from aicsimageio.readers import CziReader
+from aicspylibczi import CziFile
 
-from napari_czi_reader.czi_reader_plugin.czi_metadata_processor import extract_key_metadata
-from napari_czi_reader.czi_reader_plugin.metadata_dump import metadata_dump
+from napari_czi_reader.czi_metadata_processor import (extract_key_metadata)
+from napari_czi_reader.metadata_dump import metadata_dump
+
+# CONFIG
+DEBUG = False
+CHANNEL_NAME = False
 
 
 def truncate_filename(filename, max_chars, split_before_max=True):
@@ -54,10 +59,6 @@ def read_czi(path):
     reader = CziReader(path)
     file_name = os.path.basename(path)
     channels = reader.dims.C
-    try:
-        mosaics = reader.dims.M
-    except AttributeError:
-        mosaics = 0
 
     try:
         # metadata_list = extract_key_metadata(reader, channels)
@@ -67,35 +68,31 @@ def read_czi(path):
 
     file_name_trunked = truncate_filename(file_name, 20)
 
-
+    czi = CziFile(path)
     layer_data_list = []
     for channel in range(channels):
         metadata = metadata_list[channel]
-        print(f"Dev | Channel: {channel}, Dims: {reader.dims}")
+        if DEBUG:
+            print(f"Debug | Channel: {channel}, Dims: {reader.dims}")
 
-        # if mosaics > 0:
-        #     for m in range(reader.dims.M):
-        #         tile = reader.get_image_data("YX")
-        #         print(f"tile {m} shape:", tile.shape)
-        # else:
-        #     data = reader.get_image_data("ZYX", C=channel)
-        data = reader.get_image_data()
+        if czi.is_mosaic():
+            data = czi.read_mosaic(C=channel)  # shape: (T, Z, Y, X)
+        else:
+            data = reader.get_image_data("ZYX", C=channel)
         data = np.squeeze(data)
 
-
-
+        if DEBUG:
+            print(f"[*] Metadata dump for channel: {channel}, size: {data.shape}")
+            print(json.dumps(metadata_list, indent=2))
 
         if channels > 0:
             try:
                 metadata["name"] = f'{int(float(metadata["metadata"]["EmissionWavelength"]))}λ - {file_name_trunked}'
             except KeyError:
-                metadata["name"] = f"Channel {channel} | {file_name_trunked}"
+                metadata["name"] = f"C_{channel} - {file_name_trunked}"
 
         if not isinstance(metadata, dict):  # Holy shit, I'm making errors
             raise ValueError(f"Metadata for channel {channel} is not a dictionary. Got {type(metadata)}")
-
-        print(f"[*] Metadata dump for channel: {channel}, size: {data.shape}")
-        print(json.dumps(metadata_list, indent=2))
 
         if reader.dims.Z == 2:
             layer_data_list.append((data, metadata, "label"))
